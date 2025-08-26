dbe09c88dede35ba2e5da666007058199543b587
Denny Subke <155585746+dennysubke@users.noreply.github.com>
2025-08-26 23:18:45 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -31,7 +31,7 @@ fastify.get('/', function (req, reply) {
 const start = async () => {
   try {
     await fastify.listen(3000);
-    console.log(' Serving to http://localhost:3000/');
+    console.log(' Serving to http://0.0.0.0:3000/');
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
